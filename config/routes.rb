98e29fed89fce74e5d2734470e55d9065e67b972
Kenny <christian.koller@gmx.ch>
2013-05-06 19:45:43 +0200

    until chapter 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Depot::Application.routes.draw do
-  resources :products
+  resources :line_items
+
+
+  resources :carts
+
 
   get "store/index"
+    
+  resources :products
     
   # The priority is based upon order of creation:
   # first created -> highest priority.
